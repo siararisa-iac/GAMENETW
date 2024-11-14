5353e91d255a8b6eb60ac2c082c816aef05c4701
arisa-gluegames <arisa@glue.games>
2024-11-14 22:33:14 +0800

    Added lobby

---
+++ Assets/Scripts/Lobby/LobbyManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class LobbyManager : SingletonPUN<LobbyManager>
+{
+    [SerializeField]
+    private Text uiLogger;
+    [SerializeField]
+    private string gameVersion = "1";
+    public byte MaxPlayersPerRoom { get; private set; }
+
+    [SerializeField]
+    private GameObject lobbyPanel, roomPanel;
+
+    private void Start()
+    {
+        MaxPlayersPerRoom = 4;
+        Connect();
+        ShowPanelGroup("lobbyPanel");
+        // Make sure all clients in the lobby will load the scene once LoadLevel is called
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    public void ShowPanelGroup(string panelName)
+    {
+        lobbyPanel.SetActive(panelName.Equals(lobbyPanel.name));
+        roomPanel.SetActive(panelName.Equals(roomPanel.name));
+    }
+
+    #region MonobehaviourPunCallbacks
+
+    public override void OnConnectedToMaster()
+    {
+        Log("<color=Green>Connected to Master!</color>");
+        Debug.Log("Connected to Master");
+
+        //Join the lobby
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("Disconnected because " + cause);
+    }
+
+
+    #endregion
+
+    private void Connect()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.GameVersion = gameVersion;
+    }
+
+    public void StartGame()
+    {
+        if (PhotonNetwork.IsMasterClient)
+            PhotonNetwork.LoadLevel(1);
+    }
+
+    public void Log(string message)
+    {
+        //Dont do anything if we dont any reference to the ui logger
+        if (uiLogger == null)
+            return;
+
+        uiLogger.text += System.Environment.NewLine + message;
+    }
+
+}
